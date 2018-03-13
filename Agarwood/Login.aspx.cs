f041201c10b681a85db1ee2b2a9d41e84a9ceedc
Syazwan Mustafa <Mus.Syazwan84@hotmail.com>
2018-03-13 13:03:15 +0800

    Initial Commit

--- Agarwood/Login.aspx.cs
+++ Agarwood/Login.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 
 namespace Agarwood
 {
@@ -12,6 +15,35 @@ namespace Agarwood
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Page.Validate("signin");
+
+            var identityDbContext = new IdentityDbContext("IdentityConnectionString");
+            var userStore = new UserStore<IdentityUser>(identityDbContext);
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var user = userManager.Find(txtLoginEmail.Text, txtLoginPassword.Text);
+            if (user != null)
+            {
+                //todo: log user in / instruct user to log in
+                litLoginError.Text = "Successfully Login";
+                LogUserIn(userManager, user);
+                Server.Transfer("~/admin.aspx", true);
+            }
+            else
+            {
+                litLoginError.Text = "Invalid username or password.";
+            }
+        }
+        private void LogUserIn(UserManager<IdentityUser> userManager, IdentityUser user)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+            authenticationManager.SignIn(new Microsoft.Owin.Security.AuthenticationProperties() { }, userIdentity);
+            //Note: user is automatically redirected if trying to access another page
         }
     }
 }
